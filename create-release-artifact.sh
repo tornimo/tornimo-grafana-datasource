04dc3e7cb04e7f5bc82e11b079aea00c03cdb754
tomasz.wiewiora <tomasz.wiewiora@codewise.com>
2019-09-18 15:10:41 +0200

    fixed script that creates artifact

--- create-release-artifact.sh
+++ create-release-artifact.sh
@@ -4,4 +4,4 @@ make
 cp -r dist tornimo-datasource
 zip tornimo-datasource.zip tornimo-datasource/*
 zip tornimo-datasource.zip tornimo-datasource/*/*
-rm -rf tornimo-datsource
+rm -rf tornimo-datasource.zip tornimo-datasource
